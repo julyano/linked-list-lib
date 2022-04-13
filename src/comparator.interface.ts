40afa34fb70c2192df3e0009759fbf7f764db8d7
juliano <ju.ca.mendonca@hotmail.com>
2022-04-13 00:06:21 -0300

    Addition and improvement of methods in the comparator.ts

--- src/comparator.interface.ts
+++ src/comparator.interface.ts
@@ -1,7 +1,7 @@
 export interface IComparator<T> {
     equal(a: T, b: T): boolean;
-    lessThanOrEqual(a: T, b: T): boolean;
-    strictlyLessThan(a: T, b: T): boolean;
-    moreThanOrEqual(a: T, b: T): boolean;
-    strictlyMoreThan(a: T, b: T): boolean;
+    lessThanOrEqual(a: T, b: T, akey?: string): boolean;
+    strictlyLessThan(a: T, b: T, aKey?: string): boolean;
+    moreThanOrEqual(a: T, b: T, aKey?: string): boolean;
+    strictlyMoreThan(a: T, b: T, aKey?: string): boolean;
 }
--- src/comparator.ts
+++ src/comparator.ts
@@ -1,23 +1,92 @@
 import { IComparator } from "./comparator.interface";
+import * as _ from "lodash";
 
 export class Comparator<T> implements IComparator<T> {
     equal(a: T, b: T): boolean {
-        return a === b;
+        if (this.isObject(a) || this.isObject(b)) {
+            return _.isEqual(a, b);
+        }
+
+        return a === b;        
     }
 
-    lessThanOrEqual(a: T, b: T): boolean {
+    lessThanOrEqual(a: T, b: T, aKey?: string): boolean {
+        if (this.isObject(a) || this.isObject(b)) {
+            return this.matchesLessThanOrEqual(aKey, a)(b);
+        }
+
         return a <= b;
     }
 
-    strictlyLessThan(a: T, b: T): boolean {
+    strictlyLessThan(a: T, b: T, aKey?: string): boolean {
+        if (this.isObject(a) || this.isObject(b)) {
+            return this.matchesStrictlyLessThan(aKey, a)(b);
+        }
+
         return a < b;
     }
 
-    moreThanOrEqual(a: T, b: T): boolean {
+    moreThanOrEqual(a: T, b: T, aKey?: string): boolean {
+        if (this.isObject(a) || this.isObject(b)) {
+            return this.matchesMoreThanOrEqual(aKey, a)(b);
+        }
+
         return a >= b;
     }
 
-    strictlyMoreThan(a: T, b: T): boolean {
+    strictlyMoreThan(a: T, b: T, aKey?: string): boolean {
+        if (this.isObject(a) || this.isObject(b)) {
+            return this.matchesStrictlyMoreThan(aKey, a)(b);
+        }
+
         return a > b;
+    }
+
+    private isObject(value: any) {
+        return value !== null && typeof value === 'object'
+    }
+
+    private matchesLessThanOrEqual(key: string, srcValue: any) {
+        return (object: any) => {
+            if (object == null) {
+                return false
+            }
+
+            return object[key] <= srcValue &&
+                (srcValue !== undefined || (key in Object(object)))
+        }
+    }
+
+    private matchesMoreThanOrEqual(key: string, srcValue: any) {
+        return (object: any) => {
+            if (object == null) {
+                return false
+            }
+
+            return object[key] >= srcValue &&
+                (srcValue !== undefined || (key in Object(object)))
+        }
+    }
+
+    private matchesStrictlyMoreThan(key: string, srcValue: any) {
+        return (object: any) => {
+            if (object == null) {
+                return false
+            }
+
+            return object[key] > srcValue &&
+                (srcValue !== undefined || (key in Object(object)))
+        }
+    }
+
+    private matchesStrictlyLessThan(key: string, srcValue: any) {
+        return (object: any) => {
+            if (object == null) {
+                return false
+            }
+
+            return object[key] < srcValue &&
+                (srcValue !== undefined || (key in Object(object)))
+        }
     }
 }
